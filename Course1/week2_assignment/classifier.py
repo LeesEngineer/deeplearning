4646079804b86b8de75dd3540357ea11d50ef9e6
LeesEngineer <1140801797@qq.com>
2025-02-09 22:13:52 +0800

    Update classifier.py

--- Course1/week2_assignment/classifier.py
+++ Course1/week2_assignment/classifier.py
@@ -1,5 +1,3 @@
-#Initializing parameters, Calculating the cost function and its gradient, Using an optimization algorithm (gradient descent)
-
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
@@ -8,21 +6,148 @@ from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
 
-# Loading the data (cat/non-cat)
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
-# (m_train, num_px, num_px, 3)
 m_train = train_set_x_orig.shape[0]
 m_test = test_set_x_orig.shape[0]
 num_px = train_set_x_orig.shape[1]
 
-# _orig need to be preprocessed
-# A trick when you want to flatten a matrix X of shape (a,b,c,d) to a matrix X_flatten of shape (b*c*d, a) is to use:
 train_set_x_flatten = train_set_x_orig.reshape(m_train, -1).T
 
-# One common preprocessing step in machine learning is to center and standardize your dataset
 train_set_x = train_set_x_flatten / 255
 test_set_x = test_set_x_flatten / 255
+
+def sigmoid(z):
+    s = 1.0 / (1.0 + np.exp(-1.0 * z))
+    return s
+
+def initialize_with_zeros(dim):
+    w = np.zeros((dim, 1))
+    b = 0
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+
+    return w, b
+
+def propagate(w, b, X, Y)
+    m = X.shape[1]
+
+    # FORWARD PROPAGATION
+    A = sigmoid(np.dot(w.T, X) + b)
+    cost = -(1.0 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))
+
+    #BACKWARD PROPAGATION
+    dw = (1.0 / m) * np.dot(X, (A - Y).T)
+    db = (1.0 / m) * np.sum(A - Y)
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return grads, cost
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    costs = []
+
+    for i in range(num_iterations):
+        grads, cost = propagate(w, b, X, Y)
+
+        dw = grads["dw"]
+        db = grads["db"]
+
+        w = w - learning_rate * dw
+        b = b - learning_rate * db
+
+        if i % 100 == 0:
+            costs.append(cost)
+
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration %i: %f" %(i, cost))
+
+    params = {"w": w,
+              "b": b}
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return params, grads, costs
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+
+    A = sigmoid(np.dot(w.T, X) + b)
+
+    for i in range(A.shape[1]):
+        if A[0, i] > 0.5 :
+            Y_prediction[0, i] = 1
+        else:
+            Y_prediction[0, i] = 0
+
+    assert(Y_prediction.shape == (1, m))
+
+    return Y_prediction
+
+def model(X_train, Y_train, X_test, Y_test, num_iteration = 2000, learning_rate = 0.5, print_cost = False)
+    w, b = initialize_with_zeros(X_train.shape[0])
+
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+
+    w = parameters["w"]
+    b = parameters["b"]
+
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test,
+         "Y_prediction_train": Y_prediction_train,
+         "w": w,
+         "b": b,
+         "learning_rate": learning_rate,
+         "num_iterations": num_iterations}
+
+    return d
+
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+
+"""
+Cost after iteration 0: 0.693147
+Cost after iteration 100: 0.584508
+Cost after iteration 200: 0.466949
+Cost after iteration 300: 0.376007
+Cost after iteration 400: 0.331463
+Cost after iteration 500: 0.303273
+Cost after iteration 600: 0.279880
+Cost after iteration 700: 0.260042
+Cost after iteration 800: 0.242941
+Cost after iteration 900: 0.228004
+Cost after iteration 1000: 0.214820
+Cost after iteration 1100: 0.203078
+Cost after iteration 1200: 0.192544
+Cost after iteration 1300: 0.183033
+Cost after iteration 1400: 0.174399
+Cost after iteration 1500: 0.166521
+Cost after iteration 1600: 0.159305
+Cost after iteration 1700: 0.152667
+Cost after iteration 1800: 0.146542
+Cost after iteration 1900: 0.140872
+train accuracy: 99.04306220095694 %
+test accuracy: 70.0 %
+"""
+
+
+    
+ 
 
 
 
